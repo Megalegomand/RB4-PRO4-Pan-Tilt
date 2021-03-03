fd8b2423c344a64ab98f28b1cd9528c6dc3d2f13
Simon C. Dockweiler <scdockweiler@gmail.com>
2021-03-03 09:28:00 +0100

    Added spi files

---
+++ Tiva/spi.h
@@ -0,0 +1,31 @@
+/********************************************** 
+* Univeristy of Southern Denmark 
+* Embedded Programming (EMP) 
+* 
+* MODULENAME: spi
+* PROJECT: spi
+* DESCRIPTION: See module specification file (.h-file) 
+* Change log: 
+********************************************** 
+* Date Ud Change 
+* YYMMDD 
+* ---------------- 
+* 3. mar. 2021 PO Module created. 
+* 
+***********************************************/
+
+/***************** Header *********************/ 
+/***************** Include files **************/ 
+/***************** Defines ********************/ 
+#ifndef SPI_H_
+#define SPI_H_
+/***************** Variables ******************/ 
+/***************** Functions ******************/
+/********************************************** 
+* Input: 
+* Output: 
+* Function: 
+***********************************************/ 
+
+#endif /* SPI_H_ */
+/***************** End of module **************/
