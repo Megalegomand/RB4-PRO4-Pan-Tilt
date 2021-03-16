1b76ce67a43065466544058fcbf5978f19b85a83
Simon C. Dockweiler <scdockweiler@gmail.com>
2021-03-16 10:58:13 +0100

    Fixede SPI

--- FPGA/FPGA.srcs/sources_1/new/spi.vhd
+++ FPGA/FPGA.srcs/sources_1/new/spi.vhd
@@ -27,7 +27,6 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 use IEEE.MATH_REAL.ALL;
 
-
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
 --library UNISIM;
@@ -37,8 +36,9 @@ entity spi is
     Generic (
         register_bits : POSITIVE := 8
     );
-    Port ( data_in : in STD_LOGIC_VECTOR(register_bits-1 downto 0);
-           data_out : in STD_LOGIC_VECTOR(register_bits-1 downto 0);
+    Port ( rst : in STD_LOGIC;
+           data_in : in STD_LOGIC_VECTOR(register_bits-1 downto 0);
+           data_out : out STD_LOGIC_VECTOR(register_bits-1 downto 0);
            sclk : in STD_LOGIC;
            ss : in STD_LOGIC;
            mosi : in STD_LOGIC;
@@ -54,22 +54,57 @@ architecture Behavioral of spi is
     
     type    STATE_TYPE      is  (s_idle, s_read, s_write);    --  add states here
     signal  current_state   :   STATE_TYPE  :=  s_idle;
+    
+    signal kage : STD_LOGIC_VECTOR(reg_n_bits-1 downto 0);
 begin
 
-    process(ss, sclk)
+    next_state : process (ss, sclk, rst)
     begin
-        if (ss'event and ss = '0') then
-            reg <= data_in;
-            miso <= data_in(register_bits-1);
-        end if;
-        if (unsigned(reg_n) /= register_bits) then
-            if (sclk'event and sclk = '1') then
-                reg <=  reg(register_bits-2 downto 0) & mosi;
+        if (rst = '1') then
+            current_state <= s_idle;
+            data_out <= (others => '0');
+            reg <= (others => '0');
+            reg_n <= (others => '1');
+        elsif (falling_edge(ss)) then
+            if (current_state = s_idle) then
+                current_state <= s_read;
+                reg_n <= (others => '1');
+                reg <= data_in;
+                miso <= data_in(register_bits-1);
             end if;
-            if (sclk'event and sclk = '0') then
-                miso <= reg(register_bits-1);
+        elsif (rising_edge(sclk)) then
+            if (current_state = s_read) then
+                current_state <= s_write;
+                reg <=  reg(register_bits-2 downto 0) & mosi;
+                reg_n <= STD_LOGIC_VECTOR(unsigned(reg_n) + 1);
+            end if;
+        elsif (falling_edge(sclk)) then
+            if (current_state = s_write) then
+                if (reg_n = STD_LOGIC_VECTOR(to_unsigned(register_bits-1, reg_n_bits))) then
+                    current_state <= s_idle;
+                    data_out <= reg;
+                else
+                    current_state <= s_read;
+                    miso <= reg(register_bits-1);
+                end if;
             end if;
         end if;
     end process;
+
+--    process(ss, sclk)
+--    begin
+--        if (ss'event and ss = '0') then
+--            reg <= data_in;
+--            miso <= data_in(register_bits-1);
+--        end if;
+--        if (unsigned(reg_n) /= register_bits) then
+--            if (sclk'event and sclk = '1') then
+--                reg <=  reg(register_bits-2 downto 0) & mosi;
+--            end if;
+--            if (sclk'event and sclk = '0') then
+--                miso <= reg(register_bits-1);
+--            end if;
+--        end if;
+--    end process;
 
 end Behavioral;
