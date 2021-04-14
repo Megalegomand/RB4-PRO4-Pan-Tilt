d55da76054e5cf8bc701372c41cffcf607d6c1c1
pvbrodersen <phbro19@student.sdu.dk>
2021-04-14 11:05:33 +0200

    Retter fejl i filer
    Har rettet fejl i spi, main osv.

--- Tiva/main.c
+++ Tiva/main.c
@@ -3,14 +3,66 @@
 #include "tm4c123gh6pm.h"
 #include "emp_type.h"
 #include "spi.h"
-/**
- * main.c
- */
+#include "FreeRTOS.h"
+#include "task.h"
+#include "uart.h"
+#include "systick_frt.h"
+/*****************************************************************************
+ *   Input    : -
+ *   Output   : -
+ *   Function : SSI0 FIFO Full. Send to UART TX
+ ******************************************************************************/
+void vSpi2UART()
+{
+    while (1)
+    {
+        if (SSI0_SR_R & SSI_SR_RNE)
+        {
+            if (uart0_tx_rdy())
+            {
+                uart0_putc(spi_read());
+            }
+        }
+    }
+}
+
+/*****************************************************************************
+ *   Input    : -
+ *   Output   : -
+ *   Function : UART has data, send to SPI
+ ******************************************************************************/
+void vUART2spi()
+{
+    while (1)
+    {
+        if (uart0_rx_rdy())
+        {
+            spi_write(uart0_getc());
+        }
+    }
+}
+
 int main(void)
 {
+    // Initialzation
     spi_init();
-    while(1) {
-        spi_write(0b11101000);
-    }
-    return(0);
+    uart0_init(9600, 8, 1, 0);
+    // Create tasks
+    // SPI to UART task
+    xTaskCreate(vSpi2UART, "SPI2UART", configMINIMAL_STACK_SIZE, NULL, 0,
+                NULL);
+
+// UART to SPI task
+    xTaskCreate(vUART2spi, "UART2SPI", configMINIMAL_STACK_SIZE, NULL, 0,
+                NULL);
+
+    //Start scheduler
+    //
+    vTaskStartScheduler();
+
+    //while(1) {
+    //spi_write(0b11101000);
+
+    //}
+    return (0);
 }
