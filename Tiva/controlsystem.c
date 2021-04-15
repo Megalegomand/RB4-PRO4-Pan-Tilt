5d09c9cfb13cbef998c27683f6b9f6f9c51e7a95
Brodersen <phbro19@student.sdu.dk>
2021-04-15 10:51:04 +0200

    Rettet filer med controlsystem

--- Tiva/controlsystem.c
+++ Tiva/controlsystem.c
@@ -1,109 +1,124 @@
 /**********************************************
-* Univeristy of Southern Denmark
-* Embedded Programming (EMP)
-*
-* MODULENAME: spi
-* PROJECT: spi
-* DESCRIPTION: See module specification file (.h-file)
-* Change log:
-**********************************************
-* Date Ud Change
-* YYMMDD
-* ----------------
-* 3. mar. 2021 PO Module created.
-*
-***********************************************/
+ * Univeristy of Southern Denmark
+ * Embedded Programming (EMP)
+ *
+ * MODULENAME: spi
+ * PROJECT: spi
+ * DESCRIPTION: See module specification file (.h-file)
+ * Change log:
+ **********************************************
+ * Date Ud Change
+ * YYMMDD
+ * ----------------
+ * 3. mar. 2021 PO Module created.
+ *
+ ***********************************************/
 
 /***************** Header *********************/
 /***************** Include files **************/
 #include "controlsystem.h"
 /***************** Defines ********************/
+PIDController controllers[NUMBER_OF_CONTROLLERS];
 /***************** Constants ******************/
-
+#define PID_PAN 0
+#define PID_TIL 1
 /***************** Variables ******************/
 /**********************************************
-Functions: See module specification (h.file)
-***********************************************/
+ Functions: See module specification (h.file)
+ ***********************************************/
 /**********************************************
-* Input: N/A
-* Output: read_position
-* Function: getPosition()
-***********************************************/
-void pid_init(PIDController *pid){
-    pid -> integrator = 0.0f;
-    pid -> prev_error = 0.0f;
+ * Input: N/A
+ * Output: read_position
+ * Function: getPosition()
+ ***********************************************/
+void pid_init(INT8U pid)
+{
+    // Init Pan
+    controllers[pid].integrator = 0.0f;
+    controllers[pid].prev_error = 0.0f;
 
-    pid -> differentiator = 0.0f;
-    pid -> prev_position = 0.0f;
+    controllers[pid].differentiator = 0.0f;
+    controllers[pid].prev_position = 0.0f;
 
-    pid -> out = 0.0f;
- }
+    controllers[pid].out = 0.0f;
+}
 /**********************************************
-* Input: N/A
-* Output: controlvariable
-* Function: PID();
-***********************************************/
-float update_pid(PIDController *pid, float setpoint, float position){
+ * Input: N/A
+ * Output: controlvariable
+ * Function: PID();
+ ***********************************************/
+float update_pid(INT8U pid, float setpoint, float position)
+{
     /* error */
-    float error = setpoint-position;
+    float error = setpoint - position;
     /* Proportional term */
-    float p_term = pid->Kp * error;
+    float p_term = controllers[pid].Kp * error;
     /* integrator term */
-    pid->integrator = pid->integrator * 0.5f * pid->Ki * pid->T *(error + pid->prev_error);
-
+    controllers[pid].integrator = controllers[pid].integrator * 0.5f
+            * controllers[pid].Ki * controllers[pid].t
+            * (error + controllers[pid].prev_error);
     /* compute integrator limits */
-    float lim_min_int,lim_max_int;
-
-    if(pid->lim_max > p_term){
-        lim_max_int = pid->lim_max - p_term;
+    float lim_min_int, lim_max_int;
+    if (controllers[pid].lim_max > p_term)
+    {
+        lim_max_int = controllers[pid].lim_max - p_term;
     }
     else
     {
-        lim_max_int=0.0f:
-    }
-    if(pid->lim_min < p_term){
-        lim_min_int = pid->lim_min - p_term;
-    }
-    else
-    {
-        lim_min_int=0.0f;
-    }
+        lim_max_int = 0.0f;
 
-    /* Clamp the integrator (anti-windup) */
+}
+if (controllers[pid].lim_min < p_term)
+{
+    lim_min_int = controllers[pid].lim_min - p_term;
+}
+else
+{
+    lim_min_int = 0.0f;
+}
 
-    if(pid->integrator > lim_max_int){
-        pid_integrator = lim_max_int;
-    }
-    else if (pid->integrator < lim_min_int){
-        pid->integrator = lim_min_int;
-    }
+/* Clamp the integrator (anti-windup) */
 
-    /* Derivative (band-limited differentiator) */
+if (controllers[pid].integrator > lim_max_int)
+{
+    controllers[pid].integrator = lim_max_int;
+}
+else if (controllers[pid].integrator < lim_min_int)
+{
+    controllers[pid].integrator = lim_min_int;
+}
 
-   pid->differentiator = -(2.0f * pid->Kd * (position - pid->prev_position)   /* Note: derivative on measurement, therefore minus sign in front of equation! */
-                            + (2.0f * pid->tau - pid->T) * pid->differentiator)
-                            / (2.0f * pid->tau + pid->T);
+/* Derivative (band-limited differentiator) */
 
-   /* Compute output and apply limits */
-   pid->out = proportional + pid->integrator + pid->differentiator;
+controllers[pid].differentiator = -(2.0f * controllers[pid].Kd
+        * (position - controllers[pid].prev_position)
+        + (2.0f * controllers[pid].tau - controllers[pid].t)
+                * controllers[pid].differentiator)
+        / (2.0f * controllers[pid].tau + controllers[pid].t); /* Note: derivative on measurement, therefore minus sign in front of equation! */
 
-   if (pid->out > pid->lim_max) {
+/* Compute output and apply limits */
+controllers[pid].out = p_term + controllers[pid].integrator
+        + controllers[pid].differentiator;
 
-       pid->out = pid->lim_max;
+if (controllers[pid].out > controllers[pid].lim_max)
+{
 
-   } else if (pid->out < pid->lim_min) {
+    controllers[pid].out = controllers[pid].lim_max;
 
-       pid->out = pid->lim_min;
+}
+else if (controllers[pid].out < controllers[pid].lim_min)
+{
 
-   }
+    controllers[pid].out = controllers[pid].lim_min;
 
-   /* Store error and measurement for later use */
-   pid->prev_error       = error;
-   pid->prev_position = position;
+}
 
-   /* Return controller output */
-   return pid->out;
+/* Store error and measurement for later use */
+controllers[pid].prev_error = error;
+controllers[pid].prev_position = position;
+
+/* Return controllers output */
+return controllers[pid].out;
 }
 /***************** End of module **************/
-
 
